442636f8d695d93f2cb340518e73d1d730165202
KarlEvald <67961718+KarlEvaldNav@users.noreply.github.com>
2023-06-26 12:40:10 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+val javaVersion = JavaVersion.VERSION_17
+val mockkVersion = "1.13.5"
+
+plugins {
+    application
+    kotlin("jvm") version "1.8.22"
+    id("io.gitlab.arturbosch.detekt") version "1.23.0"
+    id("ca.cutterslade.analyze") version "1.9.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://packages.confluent.io/maven/")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation(kotlin("stdlib"))
+    implementation("ch.qos.logback:logback-classic:1.4.8")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+    implementation("org.jetbrains:annotations:24.0.1")
+    // implementation("com.github.navikt:rapids-and-rivers:2022112407251669271100.df879df951cf")
+    implementation("com.natpryce:konfig:1.6.10.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("io.mockk:mockk-dsl-jvm:$mockkVersion")
+    testImplementation("org.skyscreamer:jsonassert:1.5.1")
+}
+
+configurations.all {
+    // exclude JUnit 4
+    exclude(group = "junit", module = "junit")
+}
+
+application {
+    mainClass.set("no.nav.tiltakspenger.ApplicationKt")
+}
+
+java {
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    config = files("$projectDir/config/detekt.yml")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = javaVersion.toString()
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = javaVersion.toString()
+        kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
+    }
+    test {
+        // JUnit 5 support
+        useJUnitPlatform()
+        // https://phauer.com/2018/best-practices-unit-testing-kotlin/
+        systemProperty("junit.jupiter.testinstance.lifecycle.default", "per_class")
+    }
+    analyzeClassesDependencies {
+        warnUsedUndeclared = true
+        warnUnusedDeclared = true
+    }
+    analyzeTestClassesDependencies {
+        warnUsedUndeclared = true
+        warnUnusedDeclared = true
+    }
+}
