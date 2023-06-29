8475edb2520275eec4785f9dbcb8196122d4c53b
KarlEvaldNav <karl.evald.wigestrand@nav.no>
2023-06-29 10:58:48 +0200

    Fjerner wildcard-imports..
    Co-authored-by: richardmartinsen <Richard.Martinsen@nav.no>

--- src/main/kotlin/no/nav/tiltakspenger/tiltak/routes/HealthRoutes.kt
+++ src/main/kotlin/no/nav/tiltakspenger/tiltak/routes/HealthRoutes.kt
@@ -1,8 +1,9 @@
 package no.nav.tiltakspenger.tiltak.routes
 
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.server.application.call
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
 
 fun Route.healthRoutes() {
     get("/isalive") {
