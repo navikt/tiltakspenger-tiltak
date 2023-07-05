c41887ab60c4ebd81476397027fd4443e79fbc9a
Richard Martinsen <github@martinsen.org>
2023-07-05 11:48:08 +0200

    https -> http

--- src/main/kotlin/no/nav/tiltakspenger/tiltak/Configuration.kt
+++ src/main/kotlin/no/nav/tiltakspenger/tiltak/Configuration.kt
@@ -41,7 +41,7 @@ object Configuration {
         mapOf(
             "application.profile" to Profile.DEV.toString(),
             "logback.configurationFile" to "logback.xml",
-            "KOMET_URL" to "https://amt-tiltak.amt",
+            "KOMET_URL" to "http://amt-tiltak.amt",
             "KOMET_SCOPE" to "api://dev-gcp.amt.amt-tiltak/.default",
             "VALP_URL" to "https://valp.intern.dev.nav.no",
             "VALP_SCOPE" to "api://dev-gcp.team-valp.valp-app/.default",
