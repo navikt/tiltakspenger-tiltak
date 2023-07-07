d34d6b266923fd1621f1f42903bff211ccc1e229
Richard Martinsen <github@martinsen.org>
2023-07-07 14:31:26 +0200

    Legg til logging av response

--- src/main/kotlin/no/nav/tiltakspenger/tiltak/routes/Routes.kt.kt
+++ src/main/kotlin/no/nav/tiltakspenger/tiltak/routes/Routes.kt.kt
@@ -5,7 +5,10 @@ import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import mu.KotlinLogging
 import no.nav.tiltakspenger.tiltak.services.RoutesService
+
+val securelog = KotlinLogging.logger("tjenestekall")
 
 fun Route.routes(
     routesService: RoutesService,
@@ -13,6 +16,9 @@ fun Route.routes(
     get("/test/") {
         val ident = call.request.queryParameters["ident"] ?: "09015607561"
         val response = routesService.hentTiltak(ident)
+
+        securelog.info { response }
+
         call.respond(message = response, status = HttpStatusCode.OK)
     }
 }
