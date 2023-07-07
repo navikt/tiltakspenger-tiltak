19f3a1dccd95e1c4c9bc5e6f65c4836d6c3ef402
Richard Martinsen <github@martinsen.org>
2023-07-07 14:55:45 +0200

    Legg til mulighet for serializering

--- src/main/kotlin/no/nav/tiltakspenger/tiltak/Application.kt
+++ src/main/kotlin/no/nav/tiltakspenger/tiltak/Application.kt
@@ -1,8 +1,14 @@
 package no.nav.tiltakspenger.tiltak
 
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import io.ktor.serialization.jackson.jackson
 import io.ktor.server.application.Application
+import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.routing.routing
 import mu.KotlinLogging
 import no.nav.tiltakspenger.tiltak.Configuration.kjøreMiljø
@@ -57,10 +63,21 @@ fun Application.tiltak(
 fun Application.setupRouting(
     routesService: RoutesService,
 ) {
+    jacksonSerialization()
     routing {
         healthRoutes()
         if (kjøreMiljø() != Profile.PROD) {
             routes(routesService)
+        }
+    }
+}
+
+fun Application.jacksonSerialization() {
+    install(ContentNegotiation) {
+        jackson {
+            configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            registerModule(JavaTimeModule())
+            registerModule(KotlinModule.Builder().build())
         }
     }
 }
