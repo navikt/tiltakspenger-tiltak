58761d246d4156d1b066674c595e3ef5adc73ca3
Richard Martinsen <github@martinsen.org>
2023-10-30 13:20:50 +0100

    B Legg til endepunkt for enklere å kunne teste med tokendings

--- src/main/kotlin/no/nav/tiltakspenger/tiltak/routes/TokenxRoutes.kt
+++ src/main/kotlin/no/nav/tiltakspenger/tiltak/routes/TokenxRoutes.kt
@@ -4,11 +4,17 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.principal
+import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import mu.KotlinLogging
 import no.nav.tiltakspenger.tiltak.services.RoutesService
+
+data class RequestBody(
+    val ident: String,
+)
 
 fun Route.tokenxRoutes(
     routesService: RoutesService,
@@ -17,6 +23,14 @@ fun Route.tokenxRoutes(
 
     get("/tokenx/tiltak") {
         val ident = requireNotNull(call.principal<JWTPrincipal>()?.getClaim("pid", String::class)) { "pid er null i token" }
+        val response = routesService.hentTiltak(ident)
+        securelog.info { response }
+        call.respond(message = response, status = HttpStatusCode.OK)
+    }
+
+    post("/tokenx/tiltak") {
+        val ident = call.receive<RequestBody>().ident
+
         val response = routesService.hentTiltak(ident)
         securelog.info { response }
         call.respond(message = response, status = HttpStatusCode.OK)
