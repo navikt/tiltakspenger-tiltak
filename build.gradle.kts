0e67e435a4b8c3b73bf758218cdbbe856c4c2104
John Andre Hestad <john.andre.hestad@nav.no>
2025-01-07 13:16:47 +0100

    Bump spotless til 7.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ val felleslibVersion = "0.0.326"
 plugins {
     application
     kotlin("jvm") version "2.1.0"
-    id("com.diffplug.spotless") version "6.25.0"
+    id("com.diffplug.spotless") version "7.0.0"
 }
 
 repositories {
@@ -95,6 +95,8 @@ spotless {
             .editorConfigOverride(
                 mapOf(
                     "ktlint_standard_max-line-length" to "off",
+                    "ktlint_standard_function-signature" to "disabled",
+                    "ktlint_standard_function-expression-body" to "disabled",
                 ),
             )
     }
