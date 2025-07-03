b7a522d5e972ae0ab2a2480e4b39aab04d5da37c
Tia Firing <tia.firing@nav.no>
2025-07-03 09:46:38 +0200

    1553 returnerer feilmelding

--- src/main/kotlin/no/nav/tiltakspenger/tiltak/testdata/TestdataRoute.kt
+++ src/main/kotlin/no/nav/tiltakspenger/tiltak/testdata/TestdataRoute.kt
@@ -11,7 +11,20 @@ fun Route.testdataRoutes(
 ) {
     post("/testdata/tiltaksdeltakelse/opprett") {
         val request = call.receive<OpprettTestDeltakelseRequest>()
-        val response = kometTestdataClient.opprettTiltaksdeltakelse(request)
-        call.respond(message = response, status = HttpStatusCode.OK)
+        try {
+            val response = kometTestdataClient.opprettTiltaksdeltakelse(request)
+            call.respond(message = response, status = HttpStatusCode.OK)
+        } catch (e: Exception) {
+            call.respond(
+                message = TestDeltakelseFeilmelding(
+                    e.message ?: "Noe gikk galt ved oppretting av tiltaksdeltakelse",
+                ),
+                status = HttpStatusCode.InternalServerError,
+            )
+        }
     }
 }
+
+data class TestDeltakelseFeilmelding(
+    val feilmelding: String,
+)
