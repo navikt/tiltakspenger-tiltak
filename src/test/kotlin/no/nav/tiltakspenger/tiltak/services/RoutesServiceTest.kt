07937a9092646dd5cdc340a4a935f556c0003867
Henrik Solum <henrik.solum@nav.no>
2025-12-15 18:33:03 +0100

    1829 Filtrer bort tiltaksdeltakelser hvor tom er før fom (#379)
    Co-authored-by: John Andre Hestad <john.andre.hestad@nav.no>

--- src/test/kotlin/no/nav/tiltakspenger/tiltak/services/RoutesServiceTest.kt
+++ src/test/kotlin/no/nav/tiltakspenger/tiltak/services/RoutesServiceTest.kt
@@ -10,8 +10,6 @@ import no.nav.tiltakspenger.libs.arena.tiltak.ArenaTiltaksaktivitetResponsDTO
 import no.nav.tiltakspenger.libs.arena.tiltak.ArenaTiltaksaktivitetResponsDTO.TiltakType
 import no.nav.tiltakspenger.libs.arena.tiltak.ArenaTiltaksaktivitetResponsDTO.TiltakType.AMO
 import no.nav.tiltakspenger.libs.arena.tiltak.ArenaTiltaksaktivitetResponsDTO.TiltakType.AMOB
-import no.nav.tiltakspenger.libs.arena.tiltak.ArenaTiltaksaktivitetResponsDTO.TiltakType.AMOE
-import no.nav.tiltakspenger.libs.arena.tiltak.ArenaTiltaksaktivitetResponsDTO.TiltakType.AMOY
 import no.nav.tiltakspenger.libs.arena.tiltak.ArenaTiltaksaktivitetResponsDTO.TiltakType.ARBTREN
 import no.nav.tiltakspenger.libs.arena.tiltak.ArenaTiltaksaktivitetResponsDTO.TiltakType.HOYEREUTD
 import no.nav.tiltakspenger.libs.arena.tiltak.ArenaTiltaksaktivitetResponsDTO.TiltakType.KURS
@@ -318,6 +316,38 @@ internal class RoutesServiceTest {
         routesService.hentTiltakForSøknad("123", "correlationId").also {
             it.size shouldBe 8
         }
+    }
+
+    @Test
+    fun `tiltak med til og med dato satt etter fra og med dato filtreres bort`() {
+        val routesService = RoutesService(
+            kometClient = kometClient,
+            arenaClient = arenaClient,
+            gjennomforingRepo = gjennomforingRepo,
+        )
+
+        coEvery { kometClient.hentTiltakDeltagelser(any(), any()) } returns listOf(
+            kometDeltaker("ARBFORB", KometDeltakerStatusType.AVBRUTT),
+            kometDeltaker(
+                type = "ARBFORB",
+                fom = LocalDate.now(),
+                tom = LocalDate.now().minusDays(1),
+                status = KometDeltakerStatusType.AVBRUTT,
+            ),
+        )
+        coEvery { arenaClient.hentTiltakArena(any(), any()) } returns listOf(
+            arenaTiltak(ARBTREN, ArenaDeltakerStatusType.DELAVB),
+            arenaTiltak(
+                tiltak = ARBTREN,
+                fom = LocalDate.now(),
+                tom = LocalDate.now().minusDays(1),
+                status = ArenaDeltakerStatusType.DELAVB,
+            ),
+        )
+
+        val tiltakListe = routesService.hentTiltakForSøknad("123", "correlationId")
+
+        tiltakListe.size shouldBe 2
     }
 }
 
