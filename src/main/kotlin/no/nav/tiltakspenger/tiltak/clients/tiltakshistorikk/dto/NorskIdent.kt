e5d4b304f7bd2a1c1f68cd7903ad7cb6b51d49ff
Tia Firing <tia.firing@nav.no>
2026-01-19 07:22:47 +0100

    1760 retter feil i format

--- src/main/kotlin/no/nav/tiltakspenger/tiltak/clients/tiltakshistorikk/dto/NorskIdent.kt
+++ src/main/kotlin/no/nav/tiltakspenger/tiltak/clients/tiltakshistorikk/dto/NorskIdent.kt
@@ -1,5 +1,6 @@
 package no.nav.tiltakspenger.tiltak.clients.tiltakshistorikk.dto
 
-data class NorskIdent(
-    val value: String,
-)
+@JvmInline
+value class NorskIdent(val value: String) {
+    override fun toString() = "***********"
+}
