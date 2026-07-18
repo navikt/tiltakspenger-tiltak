6d5107b792240c79decc2c2c98abbf4f2193f24a
John Andre Hestad <john.andre.hestad@nav.no>
2026-07-18 20:10:34 +0200

    Legg til 2 manglende konsisttester for clock

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ val ktorVersion = "3.4.3"
 val jacksonVersion = "3.2.1"
 val jacksonAnnotationsVersion = "2.22"
 val kotestVersion = "6.2.2"
-val felleslibVersion = "0.0.863"
+val felleslibVersion = "0.0.20260718174757"
 
 plugins {
     application
@@ -70,7 +70,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     // Delte arkitekturregler; drar inn konsist transitivt (api-avhengighet). Egen versjon inntil felleslibVersion bumpes.
-    testImplementation("com.github.navikt.tiltakspenger-libs:konsist-regler:0.0.20260718163611")
+    testImplementation("com.github.navikt.tiltakspenger-libs:konsist-regler:$felleslibVersion")
     testImplementation("io.mockk:mockk:$mockkVersion")
     testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
--- src/test/kotlin/no/nav/tiltakspenger/arkitektur/FellesArkitekturKonsistTest.kt
+++ src/test/kotlin/no/nav/tiltakspenger/arkitektur/FellesArkitekturKonsistTest.kt
@@ -2,9 +2,11 @@ package no.nav.tiltakspenger.arkitektur
 
 import com.lemonappdev.konsist.api.Konsist
 import no.nav.tiltakspenger.libs.konsist.EnSetningPerLinje
+import no.nav.tiltakspenger.libs.konsist.IngenClockDefault
 import no.nav.tiltakspenger.libs.konsist.IngenJUnit4
 import no.nav.tiltakspenger.libs.konsist.IngenJackson2
 import no.nav.tiltakspenger.libs.konsist.IngenJupiterAsserts
+import no.nav.tiltakspenger.libs.konsist.IngenLocalDateTimeNow
 import no.nav.tiltakspenger.libs.konsist.IngenLokaleJacksonMappere
 import no.nav.tiltakspenger.libs.konsist.IngenNowUtenClock
 import org.junit.jupiter.api.Test
@@ -37,6 +39,16 @@ class FellesArkitekturKonsistTest {
     @Test
     fun `produksjonskode henter aldri nåtid uten Clock`() {
         IngenNowUtenClock.assert(Konsist.scopeFromProduction())
+    }
+
+    @Test
+    fun `bruk nå fra libs-common, ikke LocalDateTime-now`() {
+        IngenLocalDateTimeNow.assert(Konsist.scopeFromProduction())
+    }
+
+    @Test
+    fun `Clock-parametre har ikke default-verdi i produksjonskode`() {
+        IngenClockDefault.assert(Konsist.scopeFromProduction())
     }
 
     @Test
